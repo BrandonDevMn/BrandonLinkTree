6c3816e19e948c786938f7db8f33dc2a29292ada
brandonc <brandon.dev.mn@icloud.com>
2025-09-15 12:58:18 -0500

    add QR Generator

--- js/data.js
+++ js/data.js
@@ -10,5 +10,11 @@ const appsData = [
         icon: "./icons/easydice.png",
         url: "https://brandondevmn.github.io/EasyDice",
         description: "Simple dice rolling application"
+    },
+    {
+        name: "QR Generator",
+        icon: "./icons/qrgenerator.jpg",
+        url: "https://brandondevmn.github.io/QrGenerator",
+        description: "Generate QR codes for text and URLs"
     }
 ];
